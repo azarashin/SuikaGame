5f494162971f2d2d356fb65d16899c88b33fa37a
azarashin <azarashin1979@gmail.com>
2023-11-05 00:36:21 +0900

    ボールと管理クラスを追加。

---
+++ Assets/SuikaGame/Scripts/Ball.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SuikaGame
+{
+    public class Ball : MonoBehaviour
+    {
+        [SerializeField]
+        int _id = 0;
+
+        bool _isHit;
+        GameManager _manager;
+
+        private void Start()
+        {
+            _manager = FindObjectsByType<GameManager>(FindObjectsInactive.Include, FindObjectsSortMode.None)[0]; 
+            _isHit = false; 
+        }
+
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            Ball ball = collision.gameObject.GetComponent<Ball>();
+            if(ball != null && !ball._isHit && _id == ball._id)
+            {
+                _isHit=true;
+                _manager.CombineBalls(this, ball, _id); 
+            }
+        }
+
+    }
+}
---
+++ Assets/SuikaGame/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SuikaGame
+{
+    public class GameManager : MonoBehaviour
+    {
+        [SerializeField]
+        Ball[] _ballPrefabs;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void CombineBalls(Ball ballA, Ball ballB, int id)
+        {
+            int nextId = id + 1;
+            Vector3 pos = (ballA.transform.position + ballB.transform.position) * 0.5f;
+            Destroy(ballA.gameObject);
+            Destroy(ballB.gameObject);
+            if (nextId < _ballPrefabs.Length)
+            {
+                Instantiate(_ballPrefabs[nextId], pos, Quaternion.identity);
+            }
+        }
+    }
+}
